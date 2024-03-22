b94e903b1e9cfb6982fd8a12810c2e25752be89a
jorge <91345902+jreyesgfi@users.noreply.github.com>
2024-03-22 05:05:36 +0200

    Tokenize and stem test methods

--- python-backend/nltk_utils.py
+++ python-backend/nltk_utils.py
@@ -1,12 +1,21 @@
 import nltk
+from nltk.stem.porter import PorterStemmer
 #nltk.download('punkt') #pre-trained model
 
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
 def stem(word):
-    pass
+    return stemmer.stem(word.lower())
 
 
 def bag_of_words(tokenized_sentence, all_words):
     pass
+
+if __name__ == '__main__':
+    stemmer = PorterStemmer()
+    a="How're you doing?"
+    print(a)
+    print(tokenize(a))
+    words= ["organize","Organizes","organizing"]
+    print([stem(word) for word in words])
