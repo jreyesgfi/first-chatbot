8b90726c3121180e5135251f12920cb3136d9985
jorge <91345902+jreyesgfi@users.noreply.github.com>
2024-03-22 04:49:12 +0200

    Todos and first schema

---
+++ python-backend/nltk_utils.py
@@ -0,0 +1,12 @@
+import nltk
+#nltk.download('punkt') #pre-trained model
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    pass
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
